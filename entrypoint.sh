2fee8b2169078b84307afda9225c7ecd9c073219
Antony Teoh <antony.teoh@gmail.com>
2025-09-18 14:50:41 +0100

    PAE-230: Disable ZAP auto updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Starting ZAP in daemon mode..."
 # Start ZAP in daemon mode in the background
-/zap/zap.sh -daemon -host 0.0.0.0 -port 8080 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true -config api.key=zap-api-key &
+/zap/zap.sh -daemon -host 0.0.0.0 -port 8080 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true -config api.key=zap-api-key -silent &
 
 # Wait for ZAP to start
 echo "Waiting for ZAP to start..."
