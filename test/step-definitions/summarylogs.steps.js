6720e90ba3eee03d81f3bea2274ad08992eda1dd
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2025-11-10 17:04:59 +0000

    Remove validate endpoint journey tests

--- test/step-definitions/summarylogs.steps.js
+++ test/step-definitions/summarylogs.steps.js
@@ -4,50 +4,6 @@ import { SummaryLog } from '../support/generator.js'
 import { expect } from 'chai'
 import logger from '../support/logger.js'
 
-const setupSummaryLogWithDefaults = (context) => {
-  context.summaryLog = new SummaryLog()
-  context.summaryLog.setFileData(
-    'test-bucket',
-    'test-key',
-    'test-file-id',
-    'test-filename.xlsx'
-  )
-}
-
-Given('I have entered my summary log validation', function (dataTable) {
-  this.summaryLog = new SummaryLog()
-  const data = dataTable.rowsHash()
-  this.summaryLog.setFileData(
-    data['S3 Bucket'],
-    data['S3 Key'],
-    data.fileId,
-    data.filename
-  )
-  this.payload = this.summaryLog.toPayload()
-})
-
-Given(
-  'I have entered my summary log validation without {word}',
-  function (field) {
-    setupSummaryLogWithDefaults(this)
-    this.payload = this.summaryLog.toPayload()
-    delete this.payload[field]
-  }
-)
-
-When('I submit the summary log validation', async function () {
-  this.response = await baseAPI.post(
-    `/v1/organisation/${this.summaryLog.orgId}/registration/${this.summaryLog.regId}/summary-logs/validate`,
-    JSON.stringify(this.payload)
-  )
-})
-
-Then('I should receive a summary log validating response', async function () {
-  expect(this.response.statusCode).to.equal(202)
-  this.responseData = await this.response.body.json()
-  expect(this.responseData.status).to.equal('validating')
-})
-
 Given('I have the following summary log upload data', function (dataTable) {
   this.summaryLog = new SummaryLog()
   this.uploadData = dataTable.rowsHash()
