dc2fa7e07b701037c4f3592496f4448acfae36ff
Antony Teoh <antony.teoh@gmail.com>
2025-12-16 16:19:21 +0000

    Fix CDP Uploader infinite polling for non-existent queues

--- docker/scripts/localstack/10-setup-buckets.sh
+++ docker/scripts/localstack/10-setup-buckets.sh
@@ -9,7 +9,21 @@ export AWS_SECRET_ACCESS_KEY=test
 
 echo "[INIT SCRIPT] Creating buckets" >&2
 
+aws --endpoint-url=http://localhost:4566 s3 mb s3://cdp-uploader-quarantine
 aws --endpoint-url=http://localhost:4566 s3 mb s3://re-ex-summary-logs
+
+echo "[INIT SCRIPT] Creating queues" >&2
+
+# queues
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name cdp-clamav-results
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name cdp-uploader-scan-results-callback.fifo --attributes "{\"FifoQueue\":\"true\",\"ContentBasedDeduplication\": \"true\"}"
+
+# test harness
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name mock-clamav
+aws --endpoint-url=http://localhost:4566 s3api put-bucket-notification-configuration --bucket cdp-uploader-quarantine --notification-configuration '{"QueueConfigurations": [{"QueueArn": "arn:aws:sqs:eu-west-2:000000000000:mock-clamav","Events": ["s3:ObjectCreated:*"]}]}'
+aws --endpoint-url=http://localhost:4566 sqs create-queue --region $AWS_REGION --queue-name cdp-uploader-download-requests
+
+echo "[INIT SCRIPT] Uploading spreadsheets to S3 bucket" >&2
 
 aws s3api put-object \
   --bucket re-ex-summary-logs \
@@ -70,4 +84,3 @@ aws s3api put-object \
   --key exporter-invalid-key \
   --body /etc/localstack/init/ready.d/exporter-invalid.xlsx \
   --endpoint-url=http://localhost:4566 \
-
