876e412f5893181cd2e720ab8c97ecac6dede425
Antony Teoh <antony.teoh@gmail.com>
2026-02-02 11:36:18 +0000

    PAE-976: Enable tonnage monitoring

--- test/features/tonnagematerial.feature
+++ test/features/tonnagematerial.feature
@@ -1,8 +1,8 @@
-#@tonnagematerial
-#Feature: Tonnage material endpoint
-#
-#  Scenario: Tonnage material endpoint returns total tonnage information
-#    Given I am logged in as a service maintainer
-#    When I request the tonnage material
-#    Then the tonnage material information should be correct
-#
+@tonnagemonitoring
+Feature: Tonnage monitoring endpoint
+
+  Scenario: Tonnage monitoring endpoint returns total tonnage information
+    Given I am logged in as a service maintainer
+    When I request the tonnage monitoring
+    Then the tonnage monitoring information should be correct
+
--- test/step-definitions/tonnage.monitoring.steps.js
+++ test/step-definitions/tonnage.monitoring.steps.js
@@ -2,15 +2,15 @@ import { When, Then } from '@cucumber/cucumber'
 import { baseAPI, authClient } from '../support/hooks.js'
 import { expect } from 'chai'
 
-When('I request the tonnage material', async function () {
+When('I request the tonnage monitoring', async function () {
   this.response = await baseAPI.get(
     '/v1/tonnage-monitoring',
     authClient.authHeader()
   )
 })
 
-Then('the tonnage material information should be correct', async function () {
-  const tonnageMaterial = await this.response.body.json()
+Then('the tonnage monitoring information should be correct', async function () {
+  const tonnageMonitoring = await this.response.body.json()
   const expectedMaterials = [
     'aluminium',
     'fibre',
@@ -20,7 +20,7 @@ Then('the tonnage material information should be correct', async function () {
     'steel',
     'wood'
   ]
-  const materials = tonnageMaterial.materials
+  const materials = tonnageMonitoring.materials
   const actualMaterials = materials.map((m) => m.material)
   expect(actualMaterials.sort()).to.have.members(expectedMaterials.sort())
 
@@ -30,6 +30,8 @@ Then('the tonnage material information should be correct', async function () {
   })
 
   const calculatedTotal = materials.reduce((sum, m) => sum + m.totalTonnage, 0)
-  expect(Math.abs(tonnageMaterial.total - calculatedTotal)).to.be.lessThan(0.01)
-  expect(tonnageMaterial.total).to.be.greaterThanOrEqual(0)
+  expect(Math.abs(tonnageMonitoring.total - calculatedTotal)).to.be.lessThan(
+    0.01
+  )
+  expect(tonnageMonitoring.total).to.be.greaterThanOrEqual(0)
 })
