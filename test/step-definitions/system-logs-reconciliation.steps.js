eaf7376dde3ca4dc771f08cc3af777ddc728c0d3
Dan Sweeting <dansweeting@gmail.com>
2026-04-28 15:43:20 +0100

    PAE-1396: Add journey test for reconciliation system logs (#452)

---
+++ test/step-definitions/system-logs-reconciliation.steps.js
@@ -0,0 +1,51 @@
+import { Then, When } from '@cucumber/cucumber'
+import { expect } from 'chai'
+import { authClient, defraIdStub, eprBackendAPI } from '../support/hooks.js'
+
+When('the user discovers their organisations', async function () {
+  this.response = await eprBackendAPI.get(
+    '/v1/me/organisations',
+    defraIdStub.authHeader(this.userId)
+  )
+})
+
+Then(`the response status code is {int}`, async function (statusCode) {
+  expect(this.response.statusCode).to.equal(statusCode)
+  this.responseData = await this.response.body.json()
+})
+
+When(
+  `I search system logs for the user's email with sub-category {string}`,
+  async function (subCategory) {
+    this.response = await eprBackendAPI.post(
+      '/v1/system-logs/search',
+      JSON.stringify({ email: this.email, subCategory }),
+      authClient.authHeader()
+    )
+    expect(this.response.statusCode).to.equal(200)
+    this.systemLogsResponse = await this.response.body.json()
+  }
+)
+
+Then('I receive exactly {int} system log(s)', function (count) {
+  expect(this.systemLogsResponse.systemLogs).to.have.lengthOf(count)
+})
+
+Then('the system log event action is {string}', function (action) {
+  const [log] = this.systemLogsResponse.systemLogs
+  expect(log.event.action).to.equal(action)
+})
+
+Then('the system log context includes a linked organisation', function () {
+  const [log] = this.systemLogsResponse.systemLogs
+  expect(log.context.linked).to.not.equal(null)
+  expect(log.context.linked).to.have.property('id')
+  expect(log.context.linked).to.have.property('name')
+  expect(log.context.linked).to.have.property('orgId')
+  expect(log.context.linked).to.have.property('status')
+  expect(log.context.linked).to.have.property('linkedBy')
+  expect(log.context.linked).to.have.property('linkedAt')
+  expect(log.context.defraIdOrg).to.have.property('id')
+  expect(log.context.defraIdOrg).to.have.property('name')
+  expect(log.context.defraIdRelationships).to.have.lengthOf.at.least(1)
+})
